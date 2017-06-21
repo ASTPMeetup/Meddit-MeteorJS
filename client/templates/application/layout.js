f8593a75b49eeb306cdd29ab230f49ead167c2bd
ASTPMeetup <pretto417@gmail.com>
2017-06-21 00:20:17 -0500

    change app name

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,10 +1,10 @@
 
 // ***remove or establish default Session value for 'pageTitle' before deployment****
 
-Template.layout.helpers({
-  pageTitle: function() { return Session.get('pageTitle'); }
-})
-
-Tracker.autorun(function(){
-  console.log('New title! ' + Session.get('pageTitle'));
-});
+// Template.layout.helpers({
+//   pageTitle: function() { return Session.get('pageTitle'); }
+// })
+//
+// Tracker.autorun(function(){
+//   console.log('New title! ' + Session.get('pageTitle'));
+// });
