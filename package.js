c427fe6ed2f281f91cbd96148dd35487ac952ffd
ASTPMeetup <pretto417@gmail.com>
2017-07-07 22:42:46 -0500

    comment out Package.onTest({}) for live heroku app

--- package.js
+++ package.js
@@ -1,7 +1,6 @@
-
-Package.onTest(function (api) {
-    api.use('practicalmeteor:mocha');
-
-    // Add any files with mocha tests.
-    api.addFiles('lib/posts.tests.js');
-});
+// Package.onTest(function (api) {
+//     api.use('practicalmeteor:mocha');
+//
+//     // Add any files with mocha tests.
+//     api.addFiles('lib/posts.tests.js');
+// });
